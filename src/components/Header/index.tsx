295174e61edcf7f35ac8d60c643aafbd0a6d18ea
plmandreza <plmandreza@gmail.com>
2024-04-19 18:05:37 -0300

    add styles and phosphor icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
 import styles from "./Header.module.css"
+import { PlusCircle } from "@phosphor-icons/react"
 
 export function Header() {
     return(
         <header className={styles.header}>
-            <form>
+            <form className={styles.newTaskForm}>
                 <input type="text" placeholder="Criar tarefa" />
-                <button>Add</button>
+                <button>
+                    <PlusCircle size={20}/>
+                </button>
             </form>
         </header>
     )
