c29f381732a04aa6741d639596df09bbb4468b14
plmandreza <plmandreza@gmail.com>
2024-04-19 21:38:25 -0300

    add tasks and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header"
+import { Tasks } from "./components/Tasks"
 import "./global.css"
 
 function App() {
   return (
     <>
       <Header/>   
+      <Tasks/>
     </> 
   ) 
 }
---
+++ src/components/Tasks/index.tsx
@@ -0,0 +1,19 @@
+import styles from "./Tasks.module.css"
+
+export function Tasks() {
+    return (
+        <section className={styles.tasks}>
+            <header className={styles.header}>
+                <div>
+                    <p>Tarefas adicionadas</p>
+                    <span>5</span>
+                </div>
+
+                <div>
+                    <p className={styles.done}>Concluídas</p>
+                    <span>3/4</span>
+                </div>
+            </header>
+        </section>
+    )
+}
